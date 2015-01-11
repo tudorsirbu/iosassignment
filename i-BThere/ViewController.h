e7dfa3392baa693ca7bc2c6ed18ed5cc18888876
Tudor Sirbu <tdsirbu1@sheffield.ac.uk>
2015-01-11 12:31:54 +0000

    Added the facebook login

--- i-BThere/ViewController.h
+++ i-BThere/ViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<FBLoginViewDelegate>
+@property (strong, nonatomic) IBOutlet FBProfilePictureView *profilePictureView;
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *statusLabel;
 
+@property (weak, nonatomic) IBOutlet FBLoginView *loginButton;
 @end
